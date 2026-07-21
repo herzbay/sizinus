9d27a0923ca9f340c299e59005328ec5ec62e8c1
herzbay <bayypembela@gmail.com>
2026-07-22 01:20:20 +0700

    update

--- lib/services/session/user_session.dart
+++ lib/services/session/user_session.dart
@@ -1,63 +1,35 @@
-import 'package:flutter/foundation.dart';
-
 import '../../models/auth/user_model.dart';
 import '../../models/simulation/simulation_data.dart';
 
 class UserSession {
-
+  
   UserSession._();
 
-  static final ValueNotifier<UserModel?>
-      currentUserNotifier =
-          ValueNotifier(null);
-
-  static final ValueNotifier<SimulationData?>
-      simulationNotifier =
-          ValueNotifier(null);
-
-  static UserModel? get currentUser =>
-      currentUserNotifier.value;
-
-  static SimulationData? get simulation =>
-      simulationNotifier.value;
-
+  static UserModel? currentUser;
+  static SimulationData? simulation;
   static bool get isLoggedIn =>
       currentUser != null;
 
   static void setUser(
     UserModel user,
   ) {
-
-    currentUserNotifier.value =
-        user;
-
+    currentUser = user;
   }
 
   static void setSimulation(
     SimulationData data,
   ) {
-
-    simulationNotifier.value =
-        data;
-
+    simulation = data;
   }
 
   static void updateSimulation(
     SimulationData data,
   ) {
-
-    simulationNotifier.value =
-        data;
-
+    simulation = data;
   }
 
   static void clear() {
-
-    currentUserNotifier.value =
-        null;
-
-    simulationNotifier.value =
-        null;
-
+    currentUser = null;
+    simulation = null;
   }
 }
